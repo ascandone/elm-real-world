50b20d26f6ffb6e518ab63563b77c757dfe7540b
ascandone <alescandone@gmail.com>
2021-07-17 01:19:42 +0200

    added api endpoints

---
+++ src/Api/Profiles/Username_.elm
@@ -0,0 +1,9 @@
+module Api.Profiles.Username_ exposing (get)
+
+import Api.Internal
+import Data.Profile as Profile exposing (Profile)
+
+
+get : String -> Api.Internal.Request Profile
+get username =
+    Api.Internal.get Profile.decoder [ "profiles", username ]
---
+++ src/Api/Profiles/Username_/Follow.elm
@@ -0,0 +1,17 @@
+module Api.Profiles.Username_.Follow exposing (delete, post)
+
+import Api.Internal
+import Data.Profile as Profile exposing (Profile)
+import Data.User exposing (User)
+
+
+post : User -> String -> Api.Internal.Request Profile
+post user username =
+    Api.Internal.post Profile.decoder [ "profiles", username, "follow" ]
+        |> Api.Internal.withAuth user
+
+
+delete : User -> String -> Api.Internal.Request Profile
+delete user username =
+    Api.Internal.post Profile.decoder [ "profiles", username, "follow" ]
+        |> Api.Internal.withAuth user

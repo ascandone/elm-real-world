2a6c9260279d61e4ae4cd7aaf7dcb277eb053ad7
ascandone <alescandone@gmail.com>
2021-07-17 01:10:48 +0200

    refactored api module

---
+++ src/Api.elm
@@ -0,0 +1,66 @@
+module Api exposing
+    ( Error
+    , send
+    )
+
+import Api.Internal exposing (Request(..))
+import Http
+import Json.Decode as Dec exposing (Decoder)
+import Json.Encode exposing (Value)
+
+
+type alias Error =
+    { body : List String
+    }
+
+
+type Response data
+    = Ok data
+    | Err Error
+
+
+errorDecoder : Decoder Error
+errorDecoder =
+    Dec.map Error
+        (Dec.field "errors"
+            (Dec.field "body" (Dec.list Dec.string))
+        )
+
+
+responseDecoder : Decoder data -> Decoder (Response data)
+responseDecoder decoder =
+    Dec.oneOf
+        [ Dec.map Err errorDecoder
+        , Dec.map Ok decoder
+        ]
+
+
+send : Request data -> (Result Http.Error (Response data) -> msg) -> Cmd msg
+send (Request config) onResponse =
+    Http.request
+        { method = config.method
+        , headers =
+            case config.auth of
+                Nothing ->
+                    []
+
+                Just user ->
+                    [ Http.header "Authorization" ("Token " ++ user.token) ]
+        , url =
+            Url.Builder.crossOrigin
+                "https://conduit.productionready.io"
+                ("api" :: config.path)
+                config.queryParameters
+        , body =
+            case config.body of
+                Nothing ->
+                    Http.emptyBody
+
+                Just body ->
+                    Http.jsonBody body
+        , expect =
+            Http.expectJson onResponse
+                (responseDecoder config.decoder)
+        , timeout = Nothing
+        , tracker = Nothing
+        }
--- src/Api/Internal.elm
+++ src/Api/Internal.elm
@@ -1,48 +1,19 @@
 module Api.Internal exposing
-    ( Error
-    , Request
-    , Response(..)
+    ( Config
+    , Request(..)
     , delete
     , get
     , post
     , put
-    , send
     , withAuth
     , withBody
     , withParams
     )
 
 import Data.User exposing (User)
-import Http
-import Json.Decode as Dec exposing (Decoder)
+import Json.Decode exposing (Decoder)
 import Json.Encode exposing (Value)
 import Url.Builder exposing (QueryParameter)
-
-
-type alias Error =
-    { body : List String
-    }
-
-
-type Response data
-    = Ok data
-    | Err Error
-
-
-errorDecoder : Decoder Error
-errorDecoder =
-    Dec.map Error
-        (Dec.field "errors"
-            (Dec.field "body" (Dec.list Dec.string))
-        )
-
-
-responseDecoder : Decoder data -> Decoder (Response data)
-responseDecoder decoder =
-    Dec.oneOf
-        [ Dec.map Err errorDecoder
-        , Dec.map Ok decoder
-        ]
 
 
 type alias Config data =
@@ -53,6 +24,10 @@ type alias Config data =
     , decoder : Decoder data
     , path : List String
     }
+
+
+
+-- TODO proper incapsulation
 
 
 type Request data
@@ -77,37 +52,6 @@ withAuth x =
 withParams : List QueryParameter -> Request data -> Request data
 withParams x =
     builder (\c -> { c | queryParameters = x })
-
-
-send : Request data -> (Result Http.Error (Response data) -> msg) -> Cmd msg
-send (Request config) onResponse =
-    Http.request
-        { method = config.method
-        , headers =
-            case config.auth of
-                Nothing ->
-                    []
-
-                Just user ->
-                    [ Http.header "Authorization" ("Token " ++ user.token) ]
-        , url =
-            Url.Builder.crossOrigin
-                "https://conduit.productionready.io"
-                ("api" :: config.path)
-                config.queryParameters
-        , body =
-            case config.body of
-                Nothing ->
-                    Http.emptyBody
-
-                Just body ->
-                    Http.jsonBody body
-        , expect =
-            Http.expectJson onResponse
-                (responseDecoder config.decoder)
-        , timeout = Nothing
-        , tracker = Nothing
-        }
 
 
 request : String -> Decoder data -> List String -> Request data

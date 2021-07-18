e6fae8e059da21fb8b337575292fb976e19fa61c
ascandone <alescandone@gmail.com>
2021-07-18 14:17:56 +0200

    added feed api route

---
+++ src/Api/Articles/Feed.elm
@@ -0,0 +1,30 @@
+module Api.Articles.Feed exposing (get, limit, offset)
+
+import Api.Internal
+import Data.Article as Article
+import Url.Builder as Builder exposing (QueryParameter)
+
+
+type Param
+    = Param QueryParameter
+
+
+limit : Int -> Param
+limit =
+    Param << Builder.int "limit"
+
+
+offset : Int -> Param
+offset =
+    Param << Builder.int "offset"
+
+
+toParams : List Param -> List QueryParameter
+toParams =
+    List.map (\(Param p) -> p)
+
+
+get : List Param -> Api.Internal.Request Article.Collection
+get params =
+    Api.Internal.get Article.decoderCollection [ "articles", "feed" ]
+        |> Api.Internal.withParams (toParams params)

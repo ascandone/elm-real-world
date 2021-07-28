0c340724e6f63a0551fb389d9ee60cbb9ddf6486
ascandone <alescandone@gmail.com>
2021-07-28 22:00:39 +0200

    added put articles endpoint

--- src/Api/Articles.elm
+++ src/Api/Articles.elm
@@ -4,11 +4,15 @@ module Api.Articles exposing
     , get
     , limit
     , offset
+    , post
     , tag
     )
 
 import Api.Internal
-import Data.Article as Article
+import Data.Article as Article exposing (Article)
+import Data.User exposing (User)
+import Json.Encode as Enc exposing (Value)
+import Misc exposing (encodeMaybe)
 import Url.Builder as Builder exposing (QueryParameter)
 
 
@@ -50,3 +54,28 @@ get : List Param -> Api.Internal.Request Article.Collection
 get params =
     Api.Internal.get Article.decoderCollection [ "articles" ]
         |> Api.Internal.withParams (toParams params)
+
+
+type alias PostBody =
+    { title : String
+    , description : String
+    , body : String
+    , tagList : Maybe (List String)
+    }
+
+
+encodeBody : PostBody -> Value
+encodeBody body =
+    Enc.object
+        [ ( "title", Enc.string body.title )
+        , ( "description", Enc.string body.description )
+        , ( "body", Enc.string body.body )
+        , ( "tagList", encodeMaybe body.tagList (Enc.list Enc.string) )
+        ]
+
+
+post : User -> PostBody -> Api.Internal.Request Article
+post user body =
+    Api.Internal.post Article.decoderSingle [ "articles" ]
+        |> Api.Internal.withAuth user
+        |> Api.Internal.withBody (encodeBody body)

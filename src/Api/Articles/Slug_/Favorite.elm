5716d06036c5e9d4e3d5938084869fb40d02742b
ascandone <alescandone@gmail.com>
2021-07-28 21:15:44 +0200

    fixed api endpoint

--- src/Api/Articles/Slug_/Favorite.elm
+++ src/Api/Articles/Slug_/Favorite.elm
@@ -2,13 +2,16 @@ module Api.Articles.Slug_.Favorite exposing (delete, post)
 
 import Api.Internal
 import Data.Article as Article exposing (Article)
+import Data.User exposing (User)
 
 
-post : String -> Api.Internal.Request Article
-post slug =
+post : User -> String -> Api.Internal.Request Article
+post user slug =
     Api.Internal.post Article.decoderSingle [ "articles", slug, "favorite" ]
+        |> Api.Internal.withAuth user
 
 
-delete : String -> Api.Internal.Request Article
-delete slug =
+delete : User -> String -> Api.Internal.Request Article
+delete user slug =
     Api.Internal.delete Article.decoderSingle [ "articles", slug, "favorite" ]
+        |> Api.Internal.withAuth user

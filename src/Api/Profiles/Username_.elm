82060aab42cfbf69335d68d718554ddd52a15cca
ascandone <alescandone@gmail.com>
2021-08-01 16:15:20 +0200

    fix api decoder

--- src/Api/Profiles/Username_.elm
+++ src/Api/Profiles/Username_.elm
@@ -6,4 +6,4 @@ import Data.Profile as Profile exposing (Profile)
 
 get : String -> Api.Internal.Request Profile
 get username =
-    Api.Internal.get Profile.decoder [ "profiles", username ]
+    Api.Internal.get Profile.decoderSingle [ "profiles", username ]

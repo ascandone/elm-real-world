e462312fe21d1813da82fda127f830ef3dfaaf02
ascandone <alescandone@gmail.com>
2021-08-05 01:29:18 +0200

    added tests

--- src/Data/User.elm
+++ src/Data/User.elm
@@ -1,9 +1,12 @@
-module Data.User exposing (User, decoder, encode)
+module Data.User exposing (User, decoder, encode, specs)
 
+import Expect
+import Iso8601 exposing (decoder)
 import Json.Decode as Dec exposing (Decoder, string)
 import Json.Decode.Pipeline exposing (required)
 import Json.Encode as Enc exposing (Value)
 import Misc exposing (optionalMaybe)
+import Test exposing (Test)
 
 
 type alias User =
@@ -46,4 +49,43 @@ encode user =
                   )
                 ]
           )
+        ]
+
+
+
+-- Test
+
+
+exampleUserStr : String
+exampleUserStr =
+    """
+{
+    "user": {
+        "email": "jake@jake.jake",
+        "token": "jwt.token.here",
+        "username": "jake",
+        "bio": "I work at statefarm",
+        "image": null
+    }
+}
+"""
+
+
+exampleUser : User
+exampleUser =
+    { email = "jake@jake.jake"
+    , token = "jwt.token.here"
+    , username = "jake"
+    , bio = "I work at statefarm"
+    , image = Nothing
+    }
+
+
+specs : Test
+specs =
+    Test.concat
+        [ Test.test "decoding example from docs" <|
+            \() -> Expect.equal (Dec.decodeString decoder exampleUserStr) (Ok exampleUser)
+        , Test.test "encoding" <|
+            \() -> Expect.equal (Dec.decodeString decoder <| Enc.encode 0 <| encode exampleUser) (Ok exampleUser)
         ]
